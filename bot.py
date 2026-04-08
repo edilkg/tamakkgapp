b2e7036a1bf0446fb549a3a9205d97b56b625ef7
edilkg <sulaimanov.2002.es@gmail.com>
2026-04-08 17:47:33 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
-# ВСТАВЬ СВОЙ ТОКЕН ОТ BOTFATHER НИЖЕ (в кавычках)
-TOKEN = "8557711491:AAEqTlV4X6pA_J6hN7tVGTf4695jFsD_kS4"
-# ВСТАВЬ ССЫЛКУ ОТ VERCEL НИЖЕ (в кавычках)
+# Твои данные я взял со скриншота
+TOKEN = "8557711491:AAEqT1V4X6pA_J6hN7tVGTf4695jfSD_kS4"
 WEB_APP_URL = "https://tamakkgapp.vercel.app/"
 
 bot = Bot(token=TOKEN)
@@ -13,17 +12,16 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    # Те самые удобные кнопки
-    кнопки = [
+    buttons = [
         [InlineKeyboardButton(text="Заказать еду 🍔", web_app=WebAppInfo(url=WEB_APP_URL))],
         [InlineKeyboardButton(text="Профиль 👤", callback_data="profile")],
         [InlineKeyboardButton(text="Наши контакты 📞", callback_data="contacts")]
     ]
-    клавиатура = InlineKeyboardMarkup(inline_keyboard=кнопки)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     
     await message.answer(
         f"Привет, {message.from_user.first_name}! 👋\nМы рады видеть тебя здесь. 🍕🍔🍣\n\nПожалуйста, выбери один из пунктов:",
-        reply_markup=клавиатура
+        reply_markup=keyboard
     )
 
 async def main():
